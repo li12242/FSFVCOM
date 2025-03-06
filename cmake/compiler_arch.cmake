68def94a7af5f6e2fd9d6f1b984d28acf2ee3167
lilongxiang <lilongxiang01@ieisystem.com>
2025-03-06 02:59:00 -0500

    feature: enable CMake building
    - [x] Structure refactor: move fvcom into src/fvcom, FSFVCOM could
    support multiple programs sharing the same framework.
    - [x] FVCOM compiling flags are stored as cache variable in cmake
    file. User could open or select specific options when building.
    - [x] Add cmake modules for netcdf, metis, julian. Their search path are
      based on the `FVCOM_EXTENAL_PATH` variable.

--- cmake/compiler_arch.cmake
+++ cmake/compiler_arch.cmake
@@ -1,6 +1,6 @@
 # todo: select compiler automatically
-set( CMAKE_Fortran_COMPILER "ifx" )
-set( CMAKE_C_COMPILER       "icx" )
+# set( CMAKE_Fortran_COMPILER "ifx" )
+# set( CMAKE_C_COMPILER       "icx" )
 
 # Our own addition
 set( CMAKE_C_PREPROCESSOR       "/lib/cpp" )
@@ -13,7 +13,7 @@ set(CMAKE_Fortran_FLAGS "-g -traceback")
 if(CMAKE_Fortran_COMPILER_ID MATCHES "IntelLLVM")
    set(CMAKE_Fortran_FLAGS_DEBUG "${CMAKE_Fortran_FLAGS} -O0")
    set(CMAKE_Fortran_FLAGS_RELEASE "${CMAKE_Fortran_FLAGS_RELEASE} -align array64byte -mprefer-vector-width=512 -xcore-avx512")
-   set( CMAKE_Fortran_FLAGS_INIT    " -ip -w -ftz -align all -fno-alias   -xCORE-AVX2 -fp-model fast=2 -fimf-use-svml=true -convert big_endian" )
+   set( CMAKE_Fortran_FLAGS_INIT    " -ip -w -ftz -align all -fno-alias -xCORE-AVX2 -fp-model fast=2 -fimf-use-svml=true -convert big_endian" )
    set( CMAKE_C_FLAGS_INIT          " -w -O3 -ip -xCORE-AVX2 -fp-model fast=2 -fimf-use-svml=true" )
 elseif(CMAKE_Fortran_COMPILER_ID MATCHES "GNU")
    set(CMAKE_Fortran_FLAGS_DEBUG "${CMAKE_Fortran_FLAGS} -O0")
